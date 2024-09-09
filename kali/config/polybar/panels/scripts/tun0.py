deb4c707e9b3778a046c062363d020422c20b2be
0xsolo <53935554+0xS0l0@users.noreply.github.com>
2024-09-09 23:55:15 +0530

    Update tun0.py

--- kali/config/polybar/panels/scripts/tun0.py
+++ kali/config/polybar/panels/scripts/tun0.py
@@ -1,19 +1,23 @@
 #!/usr/bin/python3
 import re
 import subprocess
-from os import system as cmd
 
-# getting IP's and saving it in a file
-with open('ip.txt','w') as f:
-        ip = subprocess.run(['ip', 'a'],stdout=f,text=True)
+# Getting IPs and saving them in a file
+with open('ip.txt', 'w') as f:
+    subprocess.run(['ip', 'a'], stdout=f, text=True)
 
-# finding tun0 ip
-txt_file = open('ip.txt','r')
-IP = txt_file.read()
-pattern = re.compile("[10]+\.\w+\.\w{2,3}\.\w{2,3}")
+# Finding tun0 IP
+with open('ip.txt', 'r') as txt_file:
+    IP = txt_file.read()
+
+# Regular expression pattern
+pattern = re.compile(r'\b10\.\d{1,3}\.\d{1,3}\.\d{1,3}\b')
 search_tun0 = pattern.findall(IP)
-if len(search_tun0) > 0:
-	tun0_IP = search_tun0[0]
-	print(tun0_IP)
+
+if search_tun0:
+    # Print the first matched IP address
+    tun0_IP = search_tun0[0]
+    print(tun0_IP)
 else:
-	print("offline")
+    print("offline")
+	
